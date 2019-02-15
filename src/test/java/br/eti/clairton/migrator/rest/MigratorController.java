d47a4d8797823e6694f872f661af8ec9fcb9ac69
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2019-02-15 09:40:25 -0200

    override metodo in controller

--- src/test/java/br/eti/clairton/migrator/rest/MigratorController.java
+++ src/test/java/br/eti/clairton/migrator/rest/MigratorController.java
@@ -2,12 +2,16 @@ package br.eti.clairton.migrator.rest;
 
 import static br.com.caelum.vraptor.view.Results.status;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.observer.upload.UploadedFile;
 import br.eti.clairton.migrator.Config;
 import br.eti.clairton.migrator.Migrator;
 
@@ -31,5 +35,10 @@ public class MigratorController extends AbstractMigratorController {
 	@Get({ "", "/" })
 	public void ping() {
 		result.use(status()).noContent();
+	}
+
+	@Post({ "", "/" })
+	public void run(final UploadedFile file) throws IOException {
+		super.run(file);
 	}
 }

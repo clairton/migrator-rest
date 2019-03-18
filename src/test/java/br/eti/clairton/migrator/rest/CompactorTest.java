9858126fd7984d4d4da6029843da819d71a75707
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2019-03-18 09:01:31 -0300

    change class test name

--- src/test/java/br/eti/clairton/migrator/rest/CompactorTest.java
+++ src/test/java/br/eti/clairton/migrator/rest/CompactorTest.java
@@ -1,40 +1,17 @@
 package br.eti.clairton.migrator.rest;
 
-import static java.nio.file.Files.exists;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-
-import br.eti.clairton.migrator.Config;
-import br.eti.clairton.migrator.Migrator;
 
 public class CompactorTest {
 	@Test
-	public void testRun() throws IOException {
-		final String path = "src/test/resources/db/changelogs/changelog-main.xml";
-		final Config config = new Config(null, path) {
-			@Override
-			public Boolean isMigrate() {
-				return true;
-			}
-		};
-		final String address = "";
-		final String token = "";
-		final Uploader uploader = mock(Uploader.class);
-		final Compactor compactor = mock(Compactor.class);
-		when(compactor.zip(config)).thenReturn(File.createTempFile("changelog", ".zip"));
-		final Migrator migrator = new MigratorRemote(config, address, token, uploader, compactor);
-		migrator.run();
-		final ArgumentCaptor<File> file = ArgumentCaptor.forClass(File.class);
-		final ArgumentCaptor<String> string = ArgumentCaptor.forClass(String.class);
-		verify(uploader).run(file.capture(), string.capture(), string.capture());
-		assertTrue(exists(file.getValue().toPath()));
+	public void testRun() {
+		final String path = new File("src/test/resources/db/changelogs").getAbsolutePath();
+		final Compactor compactor = new Compactor();
+		final File zip = compactor.zip(path);
+		assertNotNull(zip.length());
 	}
 }
--- src/test/java/br/eti/clairton/migrator/rest/MigratorRemoteTest.java
+++ src/test/java/br/eti/clairton/migrator/rest/MigratorRemoteTest.java
@@ -1,17 +1,40 @@
 package br.eti.clairton.migrator.rest;
 
-import static org.junit.Assert.assertEquals;
+import static java.nio.file.Files.exists;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import br.eti.clairton.migrator.Config;
+import br.eti.clairton.migrator.Migrator;
 
 public class MigratorRemoteTest {
 	@Test
-	public void testRun() {
-		final String path = new File("src/test/resources/db/changelogs").getAbsolutePath();
-		final Compactor compactor = new Compactor();
-		final File zip = compactor.zip(path);
-		assertEquals(1408l, zip.length());
+	public void testRun() throws IOException {
+		final String path = "src/test/resources/db/changelogs/changelog-main.xml";
+		final Config config = new Config(null, path) {
+			@Override
+			public Boolean isMigrate() {
+				return true;
+			}
+		};
+		final String address = "";
+		final String token = "";
+		final Uploader uploader = mock(Uploader.class);
+		final Compactor compactor = mock(Compactor.class);
+		when(compactor.zip(config)).thenReturn(File.createTempFile("changelog", ".zip"));
+		final Migrator migrator = new MigratorRemote(config, address, token, uploader, compactor);
+		migrator.run();
+		final ArgumentCaptor<File> file = ArgumentCaptor.forClass(File.class);
+		final ArgumentCaptor<String> string = ArgumentCaptor.forClass(String.class);
+		verify(uploader).run(file.capture(), string.capture(), string.capture());
+		assertTrue(exists(file.getValue().toPath()));
 	}
 }
